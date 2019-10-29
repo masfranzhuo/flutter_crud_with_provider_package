28879f0588692431b023cc7b41aef37d289850cd
Masfran <masfran@xcidic.com>
2019-10-29 16:43:56 +0700

    refactor repository code

--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -1,30 +1,19 @@
 
 
 import 'package:flutter_crud_with_provider_package/dao/user_api.dart';
+import 'package:flutter_crud_with_provider_package/dao/user_dao.dart';
 import 'package:flutter_crud_with_provider_package/model/user_model.dart';
 
 class UserRepository {
-  final userApi = UserApi();
+  final userDao = UserApi(); // Use UserDao(); for SQLite
 
-  Future getUsers({String query}) => userApi.getUsers(query: query);
+  Future getUsers({String query}) => userDao.getUsers(query: query);
 
-  Future getUser(int id) => userApi.getUser(id: id);
+  Future getUser(int id) => userDao.getUser(id: id);
 
-  Future createUser(User user) => userApi.createUser(user);
+  Future createUser(User user) => userDao.createUser(user);
 
-  Future updateUser(User user) => userApi.updateUser(user);
+  Future updateUser(User user) => userDao.updateUser(user);
 
-  Future deleteUser(int id) => userApi.deleteUser(id);
-  
-  // final userDao = UserDao();
-
-  // Future getUsers({String query}) => userDao.getUsers(query: query);
-
-  // Future getUser(int id) => userDao.getUser(id: id);
-
-  // Future createUser(User user) => userDao.createUser(user);
-
-  // Future updateUser(User user) => userDao.updateUser(user);
-
-  // Future deleteUser(int id) => userDao.deleteUser(id);
+  Future deleteUser(int id) => userDao.deleteUser(id);
 }
